565dd98caf7553bd21e2950a287a46172058365d
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-21 21:14:59 +0300

    Sync mobile DEV build

--- js/mobile-bottom-nav.js
+++ js/mobile-bottom-nav.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  const DEV='dev-2026.09.21.13';
+  const DEV='dev-2026.09.21.14';
   const mobile=window.matchMedia('(max-width:560px)');
   const HALF_WIDTH=44;
   const TRANSITION='transform 200ms cubic-bezier(.22,.8,.25,1)';
